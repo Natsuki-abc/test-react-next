803fd410683ac563caea8d5d2a63386a2861e52d
natsuki <natsuki.ss@icloud.com>
2025-04-23 18:49:37 +0900

    戻し

--- app/page.js
+++ app/page.js
@@ -43,8 +43,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-
-            Read our docs aaaaa
+            Read our docs
           </a>
         </div>
       </main>
